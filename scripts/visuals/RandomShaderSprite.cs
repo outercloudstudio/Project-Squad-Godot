6e43503cb38ee03749c4c2f9c7b43b3aa7292bf6
Outer Cloud Studio <outercloudstudio@gmail.com>
2025-02-14 02:52:55 -0600

    Upgrade to 4.4 and wip per tree noise

--- scripts/visuals/RandomShaderSprite.cs
+++ scripts/visuals/RandomShaderSprite.cs
@@ -4,7 +4,8 @@ using System;
 public partial class RandomShaderSprite : Sprite2D {
 	public override void _Ready() {
 		RandomNumberGenerator random = new RandomNumberGenerator();
+		int id = random.RandiRange(0, 100);
 
-		(Material as ShaderMaterial).SetShaderParameter("id", random.RandiRange(0, 100));
+		SetInstanceShaderParameter("id", id);
 	}
 }
